c80acf79ebd78f7096838314bf4ae1000266e230
Bob Feuerstein <thebobbyllama@yahoo.com>
2022-03-11 03:13:19 -0600

    Updated profanity detection.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,28 @@
 export const detectProfanity = (input) => {
-	return !!input.match(/(\s|\.|_)b(\s|\.|_)*(i|ia|\*)(\s|\.|_)*t(\s|\.|_)*c(\s|\.|_)*h|(\s|\.|_)c(\s|\.|_)*(o|\*)c(\s|\.|_)*k|(\s|\.|_)c(\s|\.|_)*(u|\*)(\s|\.|_)*n(\s|\.|_)*t|f(\s|\.|_)*(a|\*)(\s|\.|_)*g(\s|\.|_)*g(\s|\.|_)*(o|0)(\s|\.|_)*t|f(\s|\.|_)*(u|\*)(\s|\.|_)*c(\s|\.|_)*k|p(\s|\.|_)*(e|\*)(\s|\.|_)*n(\s|\.|_)*i(\s|\.|_)*s|p(\s|\.|_)*(u|\*)(\s|\.|_)*s(\s|\.|_)*s(\s|\.|_)*y|(\s|\.|_)(s|\$)(\s|\.|_)*(h|#)(\s|\.|_)*(i|!|\*)(\s|\.|_)*t|(\s|\.|_)t(i|\*)t/gi);
+	// bitch
+	if (input.match(/\bb(\s|\.|_)*(i|ia|!|\*)(\s|\.|_)*t(\s|\.|_)*c(\s|\.|_)*h/gi)) return false;
+	// cock
+	if (input.match(/\bc(\s|\.|_)*(o|0|u|\*)c(\s|\.|_)*k/gi)) return false;
+	// cunt
+	if (input.match(/\bc(\s|\.|_)*(u|\*)(\s|\.|_)*n(\s|\.|_)*t/gi)) return false;
+	// faggot
+	if (input.match(/f(\s|\.|_)*(a|\*)(\s|\.|_)*(g|6)(\s|\.|_)*(g|6)(\s|\.|_)*(o|0)(\s|\.|_)*t/gi)) return false;
+	// fuck
+	if (input.match(/f(\s|\.|_)*(u|\*)(\s|\.|_)*c(\s|\.|_)*k/gi)) return false;
+	// n-word
+	if (input.match(/n(\s|\.|_)*(i|!|1|\*)(\s|\.|_)*(g|6)(\s|\.|_)*(g|6)(\s|\.|_)*(e|3)(\s|\.|_)*r/gi)) return false;
+	// penis
+	if (input.match(/p(\s|\.|_)*(e|3|\*)(\s|\.|_)*n(\.|_)*(i|1|!)(\s|\.|_)*(s|$)/gi)) return false;
+	// piss
+	if (input.match(/p(\s|\.|_)*(i|1|!|\*)(\s|\.|_)*(s|\$)(\s|\.|_)*(s|\$)/gi)) return false;
+	// pussy
+	if (input.match(/p(\s|\.|_)*(u|\*)(\s|\.|_)*(s|\$)(\s|\.|_)*(s|\$)(\s|\.|_)*y/gi)) return false;
+	// shit
+	if (input.match(/(\.|_)(s|\$)(\s|\.|_)*(h|#)(\s|\.|_)*(i|!|\*)(\s|\.|_)*t/gi)) return false;
+	// tit
+	if (input.match(/(\s|\.|_)t(i|\*)t/gi)) return false;
+	// twat
+	if (input.match(/\bt(\s|\.|_)*w(a|\*)(\s|\.|_)*t/gi)) return false;
+
+	return true;
 }
